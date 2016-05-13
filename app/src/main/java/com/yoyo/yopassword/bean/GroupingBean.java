b95bcf6535e81ada340e6f27c70eb7d85c6b1274
ly <ly@jianzhimao.com>
2016-05-13 18:04:09 +0800

    添加实体

---
+++ app/src/main/java/com/yoyo/yopassword/bean/GroupingBean.java
@@ -0,0 +1,31 @@
+package com.yoyo.yopassword.bean;
+
+/**
+ * 项目名称：YoPassword
+ * 类描述：分组
+ * 创建人：yoyo
+ * 创建时间：2016/5/13 17:59
+ * 修改人：yoyo
+ * 修改时间：2016/5/13 17:59
+ * 修改备注：
+ */
+public class GroupingBean {
+    int groupingId;
+    String groupingName;
+
+    public int getGroupingId() {
+        return groupingId;
+    }
+
+    public void setGroupingId(int groupingId) {
+        this.groupingId = groupingId;
+    }
+
+    public String getGroupingName() {
+        return groupingName;
+    }
+
+    public void setGroupingName(String groupingName) {
+        this.groupingName = groupingName;
+    }
+}
---
+++ app/src/main/java/com/yoyo/yopassword/bean/PasswordInfoBean.java
@@ -0,0 +1,23 @@
+package com.yoyo.yopassword.bean;
+
+import java.util.Date;
+
+/**
+ * 项目名称：YoPassword
+ * 类描述：密码的信息
+ * 创建人：yoyo
+ * 创建时间：2016/5/13 17:54
+ * 修改人：yoyo
+ * 修改时间：2016/5/13 17:54
+ * 修改备注：
+ */
+public class PasswordInfoBean {
+    int passwordInfoId;
+    String account;
+    String password;
+    String title;
+    boolean isTop;
+    long saveInfoTime;
+    String remarks;
+    int groupingId;
+}
